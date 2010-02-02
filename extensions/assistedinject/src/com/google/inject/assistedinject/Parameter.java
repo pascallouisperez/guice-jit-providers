pascallouisperez/guice-jit-providers

commit a7048f59b5e7424002f69ffd7caa02ba8620c955

fix for locking problem in old assisted inject.
git-svn-id: http://google-guice.googlecode.com/svn/trunk@1120 d779f126-a31b-0410-b53b-1d3aecad763e

extensions/assistedinject/src/com/google/inject/assistedinject/Parameter.java
@@ -39,6 +39,8 @@ class Parameter {
   private final Annotation bindingAnnotation;
   private final boolean isProvider;
 
+  private volatile Provider<? extends Object> provider;
+
   public Parameter(Type type, Annotation[] annotations) {
     this.type = type;
     this.bindingAnnotation = getBindingAnnotation(annotations);
@@ -82,9 +84,17 @@ class Parameter {
    * Returns the Guice {@link Key} for this parameter.
    */
   public Object getValue(Injector injector) {
-    return isProvider
-        ? injector.getProvider(getBindingForType(getProvidedType(type)))
-        : injector.getInstance(getPrimaryBindingKey());
+    if (null == provider) {
+      synchronized (this) {
+        if (null == provider) {
+          provider = isProvider
+              ? injector.getProvider(getBindingForType(getProvidedType(type)))
+              : injector.getProvider(getPrimaryBindingKey());
+        }
+      }
+    }
+
+    return provider.get();
   }
 
   public boolean isBound(Injector injector) {

build.properties
@@ -9,6 +9,7 @@ jndi.src.dir=extensions/jndi/src
 throwingproviders.src.dir=extensions/throwingproviders/src
 multibindings.src.dir=extensions/multibindings/src
 privatemodules.src.dir=extensions/privatemodules/src
+lifecycle.src.dir=lifecycle/src
 build.dir=build
 javadoc.packagenames=com.google.inject,com.google.inject.spi,\
   com.google.inject.matcher,\

build.xml
@@ -88,6 +88,8 @@
         <pathelement location="lib/build/junit.jar"/>
         <pathelement location="lib/build/servlet-api-2.5.jar"/>
         <pathelement location="lib/build/easymock.jar"/>
+        <pathelement location="lib/javax.inject.jar"/>
+        <pathelement location="lib/build/javax.inject-tck.jar"/>
       </classpath>
       <arg value="com.google.inject.AllTests"/>    
       <syspropertyset>
@@ -113,6 +115,7 @@
         <pathelement location="${jndi.src.dir}"/>
         <pathelement location="${throwingproviders.src.dir}"/>
         <pathelement location="${multibindings.src.dir}"/>
+        <pathelement location="${lifecycle.src.dir}"/>
       </sourcepath>
       <classpath refid="compile.classpath"/>
       <classpath>

src/com/google/inject/Injector.java
@@ -227,7 +227,7 @@ public interface Injector {
   /**
    * Returns a map containing all scopes in the injector. The maps keys are scoping annotations
    * like {@code Singleton.class}, and the values are scope instances, such as {@code
-   * Scopes.SINGLETON. The returned map is immutable.
+   * Scopes.SINGLETON}. The returned map is immutable.
    *
    * <p>This method is part of the Guice SPI and is intended for use by tools and extensions.
    */

commit 4b5df19c388cf1395f3f14084e61fdab7b9e5ddd

Fix for assisted inject bug introduced in r1120.
git-svn-id: http://google-guice.googlecode.com/svn/trunk@1139 d779f126-a31b-0410-b53b-1d3aecad763e

extensions/assistedinject/src/com/google/inject/assistedinject/Parameter.java
@@ -94,7 +94,7 @@ class Parameter {
       }
     }
 
-    return provider.get();
+    return isProvider ? provider : provider.get();
   }
 
   public boolean isBound(Injector injector) {